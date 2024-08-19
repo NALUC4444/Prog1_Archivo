d8c1636cfcafe484c6f259c51ea6176c9f79d44d
NALUC4444 <163031421+NALUC4444@users.noreply.github.com>
2024-08-19 10:00:14 -0300

    Actualizar Ejercicio11.pas

--- Practico4/Ejercicio11.pas
+++ Practico4/Ejercicio11.pas
@@ -1,24 +1,26 @@
 program P4Ej11;
 
 var
-    i, m, n : integer;
+ i, m, n, maximo : integer;
+ anteriorValor, nuevoValor : integer;
 
-begin
-    write('Ingrese el valor de m: ');
-    readln(m);
-    write('Ingrese el valor de n: ');
-    readln(n);
-    if (m > n) then
-        writeln('Vuelva a ingresar nuevos valores...')
-    else
+begin 
+ write('Ingrese m: ');
+ readln(m);
+ write('Ingrede n: ');
+ readln(n);
+ if (m > n) then
+  writeln('Valores invalidos...')
+ else (*Cierra el if*)
+  begin
+   maximo := m;
+   for i := m to n do
     begin
-        writeln('Empieza el programa...');
+     anteriorValor := sqr(i-1) - (18*(i-1)) + 5;
+     nuevoValor := sqr(i) - (18*i) + 5;
+     if (anteriorValor <=  nuevoValor) then
+      maximo := nuevoValor;
     end;
+   writeln('El valor maximo para x en el entorno', m, ' a ', n, ' es: ', maximo);
+   end; (*Cierra begin del else*)
 end.
-
-(*Letra ejercicio:
-Considere la funci ́on f tal que f(x) = x^2 − 18x +5, donde x es un valor entero en el entorno
-de m a n, siendo m y n dos enteros tales que m ≤ n.
-Escriba un programa en Pascal que lea los valores para m y n de la entrada estandar y
-despliegue en la salida estandar el valor maximo de f(x) para x en ese entorno. Incluya
-mensajes de salida con etiquetas descriptivas para solicitar y/o exhibir los valores.*)
